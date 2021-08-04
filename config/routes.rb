7bbd4a0038ccf8e01e7bad1a0071ea3425be8c6a
Claeb1mb <52614744+Claeb1mb@users.noreply.github.com>
2021-08-04 16:54:53 -0400

    complete html pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'html_pages/home'
+  root 'html_pages#home'
   get 'html_pages/help'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "html_pages/about"
+ 
 end
--- test/controllers/html_pages_controller_test.rb
+++ test/controllers/html_pages_controller_test.rb
@@ -4,10 +4,18 @@ class HtmlPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get html_pages_home_url
     assert_response :success
+    assert_select "title", "Home | Oribit Feedback Application"
   end
 
   test "should get help" do
     get html_pages_help_url
     assert_response :success
+    assert_select "title", "Help | Oribit Feedback Application"
+  end
+
+  test "shoudl get about" do 
+    get html_pages_about_url
+    assert_response :success
+    assert_select "title", "About | Oribit Feedback Application"
   end
 end
